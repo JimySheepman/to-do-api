23cd1bbf0ace5b9777ba08c3d2dd0f12ea4acb01
JimySheepman <y.alikoyuncu@gmail.com>
2022-05-12 10:47:46 +0300

    insert test data

--- db/migrations/create_comments_table.sql
+++ db/migrations/create_comments_table.sql
@@ -3,6 +3,20 @@ CREATE TABLE comments (
     task_id INTEGER NOT NULL,
     user_name VARCHAR (50) NOT NULL,
     user_comment VARCHAR (160) NOT NULL,
+    statu  VARCHAR (50) NOT NULL,
     created_at TIMESTAMP NOT NULL,
     CONSTRAINT fk_task FOREIGN KEY(task_id) REFERENCES tasks(id)
 );
+
+
+INSERT into comments (task_id,user_name ,user_comment ,statu ,created_at) 
+VALUES (1,'testUser','testComment','approved', current_timestamp);
+
+INSERT into comments (task_id,user_name ,user_comment ,statu ,created_at) 
+VALUES (1,'testUser','testComment','rejection', current_timestamp);
+
+INSERT into comments (task_id,user_name ,user_comment ,statu ,created_at) 
+VALUES (2,'testUser','testComment','approved', current_timestamp);
+
+INSERT into comments (task_id,user_name ,user_comment ,statu ,created_at) 
+VALUES (2,'testUser','testComment','rejection', current_timestamp);
--- db/migrations/create_tasks_table.sql
+++ db/migrations/create_tasks_table.sql
@@ -6,3 +6,17 @@ CREATE TABLE tasks (
     statu  VARCHAR (50) NOT NULL,
     created_at TIMESTAMP NOT NULL
 );
+
+
+INSERT into tasks (title,"content",category,statu,created_at) 
+VALUES ('Test','Test','Test','Test', current_timestamp);
+
+INSERT into tasks (title,"content",category,statu,created_at) 
+VALUES ('Test1','Test1','Test1','Test1', current_timestamp);
+
+
+INSERT into tasks (title,"content",category,statu,created_at) 
+VALUES ('Test2','Test2','Test2','Test2', current_timestamp);
+
+INSERT into tasks (title,"content",category,statu,created_at) 
+VALUES ('Test3','Test3','Test3','Test3', current_timestamp);

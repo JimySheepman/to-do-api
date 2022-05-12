f59e4e8b81185e748d51d8cbe8e6dd57d77e6bb4
JimySheepman <y.alikoyuncu@gmail.com>
2022-05-12 09:53:42 +0300

    add kafa producer and consumer

---
+++ internal/infrastructure/broker/producer/producer.go
@@ -0,0 +1,11 @@
+package producer
+
+import kafka "github.com/segmentio/kafka-go"
+
+func NewKafkaWriter(kafkaURL, topic string) *kafka.Writer {
+	return &kafka.Writer{
+		Addr:     kafka.TCP(kafkaURL),
+		Topic:    topic,
+		Balancer: &kafka.LeastBytes{},
+	}
+}

dfc1e7cfc4194b7eb751cf30c4cd28859e323a6b
JimySheepman <y.alikoyuncu@gmail.com>
2022-05-30 09:36:51 +0300

    add persistence package and configure file

--- internal/infrastructure/persistence/connect.go
+++ internal/infrastructure/persistence/connect.go
@@ -7,22 +7,15 @@ import (
 	_ "github.com/lib/pq"
 )
 
-func ConnectDB() (*sql.DB, error) {
-	var err error
-	/* 	//	TODO:every time .env file load error in dockerfile
-	   	p := config.Config("DB_PORT")
-	   	port, err := strconv.ParseUint(p, 10, 32)
-	   	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", config.Config("DB_HOST"), port, config.Config("DB_USER"), config.Config("DB_PASSWORD"), config.Config("DB_NAME"))
-	*/
-	dsn := fmt.Sprintf("postgres://postgres:root@localhost:5432/postgres?sslmode=disable")
-	db, err := sql.Open("postgres", dsn)
+func ConnectDB(driverName, dsn string) (*sql.DB, error) {
+	db, err := sql.Open(driverName, dsn)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	err = db.Ping()
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	fmt.Println("Successfully connected!")
---
+++ internal/infrastructure/persistence/connect_test.go
@@ -0,0 +1,30 @@
+package persistence
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+)
+
+func TestConnectDB(t *testing.T) {
+	tests := []struct {
+		testName   string
+		driverName string
+		dns        string
+		err        error
+	}{
+		{"succesful connection", "postgres", "postgres://postgres:root@localhost:5432/postgres?sslmode=disable", nil},
+		{"dns error", "postgres", "postgres://postgres:root@localhost/postgres?sslmode=disable", nil},
+		{"ping error", "postgres", "postgres://postgres:root@localhost:5432", errors.New(`pq: SSL is not enabled on the server`)},
+		{"driver name error", "postg", "postgres://postgres:root@localhost:5432/postgres?sslmode=disable", errors.New(`sql: unknown driver "postg" (forgotten import?)`)},
+	}
+
+	for _, test := range tests {
+		t.Run(test.testName, func(t *testing.T) {
+			_, actual := ConnectDB(test.driverName, test.dns)
+			if !reflect.DeepEqual(actual, test.err) {
+				t.Errorf("got the %v value\nexpecting the %v value", actual, test.err)
+			}
+		})
+	}
+}
